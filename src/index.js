6efa7592748f6582c93f7687564900a168fa82e5
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-13 16:22:45 -0500

    completed all MVP requirements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// importing React libs from node_modules folder (see package.json)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// 💥💥💥 Mount a React element to the DOM (only once per app) 💥💥💥
+
+// render App at the DOM element with 'root' class
 ReactDOM.render(<App />, document.getElementById('root'));
